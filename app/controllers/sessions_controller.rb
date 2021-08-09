43ba3ad4e8c458d80a6b3dd3962a0cecf4830b68
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-09 10:44:52 -0400

    commit tree b6f5869d6ff9350f8b05da714927493fcfd9e360

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   
   def index
+    # byebugq
+    
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
